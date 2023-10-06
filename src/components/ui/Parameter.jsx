a707d53b069d3dd76226efe679b3d5fe199ba3d7
Anas Kayyali <anas678911@hotmail.com>
2023-10-07 01:57:49 +0300

    [Update]: new update

--- src/components/ui/Parameter.jsx
+++ src/components/ui/Parameter.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
 
+import { Tooltip } from "antd"
+
 const Parameter = ({value,handle,label,notify}) => {
 
     const handleParameter = (e) => {
@@ -13,11 +15,28 @@ const Parameter = ({value,handle,label,notify}) => {
         handle(value)
     }
 
+    const Title = (label) => {
+        switch (label) {
+            case "x":
+                return "The x axis of the frame"
+            case "y":
+                return "The y axis of the frame"
+            case "t":
+                return "The time in seconds when the text will appear in the video"
+            case "d":
+                return "Duration of the text apperance on the video"
+            case "s":
+                return "Font size of the text"
+        }       
+    }
+
     return (
-        <main className="flex max-lg:basis-[33.3%] justify-center items-center gap-x-2">
-            <h1 className="lg:text-lg">{label}</h1>
-            <input type="text" value={value} onChange={handleParameter} className="border w-14 text-center shadow-sm rounded-md py-1" />
-        </main>
+        <Tooltip title={Title(label)}>
+            <main className="flex max-lg:basis-[33.3%] justify-center items-center gap-x-2">
+                <h1 className="lg:text-lg">{label}</h1>
+                <input type="text" value={value} onChange={handleParameter} className="border w-14 text-center shadow-sm rounded-md py-1" /> 
+            </main>
+        </Tooltip>
     )
 }
 
